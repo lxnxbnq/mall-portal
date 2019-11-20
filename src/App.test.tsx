54552772a7fa6e97c0b4ae07e5e624be69987729
wb-zqy582530 <wb-zqy582530@alibaba-inc.com>
2019-11-20 10:07:48 +0800

    init commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import App from './App'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe(`<App />`, () => {
+  const wrapper = shallow(<App />);
+  it('1. 包含一个p标签', () => {
+    expect(wrapper.find('p').length).toBe(1);
+  })
+
+  it('2. 一个header的class为App-header', ()=> {
+    expect(wrapper.find('header').hasClass('App-header')).toBeTruthy();
+  })
+
+  it('3. header有3个子元素', () => {
+    expect(wrapper.find('header').children().length).toBe(3);
+  })
+})
+
+// 当希望确保UI不会意外更改时，则可以使用snapshot来进行对比，如果快照不匹配，则会报错
+// describe(`测试<App />快照`, () => {
+//   const tree = shallow(<App />);
+//   it('1. 匹配快照', () => {
+//     expect(toJson(tree)).toMatchSnapshot();
+//   })
+// })
