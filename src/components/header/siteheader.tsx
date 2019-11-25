15cdc3a5f38f1d9be988d9e3032d1ecaf6a7d4c9
wb-zqy582530 <wb-zqy582530@alibaba-inc.com>
2019-11-25 17:56:02 +0800

    update: 修改首页

--- src/components/header/siteheader.tsx
+++ src/components/header/siteheader.tsx
@@ -3,18 +3,11 @@ import cx from 'classnames';
 import s from './siteheader.module.less';
 
 const SiteHeader: React.FunctionComponent = (): React.ReactElement => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [menuVisible, setMenuVisible] = useState(false);
 
-  const handleNavListMouseOver = (e: any) => {
-    if (e.nativeEvent.target.getAttribute('data-menu-trigger')) {
-      setMenuVisible(true);
-    } else {
-      setMenuVisible(false);
-    }
-  };
-
   return (
-    <div onMouseOver={handleNavListMouseOver} className={s['site-header']}>
+    <div className={s['site-header']}>
       <div className={s.container}>
         <div className={s['header-logo']}>
           <a className={s.logo}></a>
@@ -22,32 +15,25 @@ const SiteHeader: React.FunctionComponent = (): React.ReactElement => {
         <div className={s['header-nav']}>
           <ul className={s['nav-list']}>
             <li className={s['nav-item']}>
-              <a className={s['link']} data-menu-trigger>
-                小米手机
-              </a>
+              <a className={s['link']}>小米手机</a>
             </li>
             <li className={s['nav-item']}>
-              <a className={s['link']} data-menu-trigger>
-                Redmi红米
-              </a>
+              <a className={s['link']}>Redmi红米</a>
             </li>
             <li className={s['nav-item']}>
-              <a className={s['link']} data-menu-trigger>
-                电视
-              </a>
+              <a className={s['link']}>电视</a>
             </li>
           </ul>
         </div>
       </div>
       <div
-        data-menu-trigger
         className={
           menuVisible
             ? cx(s['header-nav-menu'], s['header-nav-menu-visible'])
             : s['header-nav-menu']
         }
       >
-        <div data-menu-trigger className={s['container']}></div>
+        <div className={s['container']}></div>
       </div>
     </div>
   );
